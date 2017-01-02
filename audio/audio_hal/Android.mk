f5a83eb81c32064628efbed731aedb055e7a5918
grace5921 <androidlover5842@gmail.com>
2017-01-02 18:12:19 +0530

    AudioHal: delete old policy hal
    it's not needed in cm13 because cm13 has his own policy manager

--- audio/audio_hal/Android.mk
+++ audio/audio_hal/Android.mk
@@ -1,7 +1,16 @@
-# Copyright 2011 The Android Open Source Project
-
-#AUDIO_POLICY_TEST := true
-#ENABLE_AUDIO_DUMP := true
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
@@ -58,38 +67,6 @@ LOCAL_C_INCLUDES += hardware/libhardware/include
 LOCAL_C_INCLUDES += hardware/libhardware_legacy/include
 LOCAL_C_INCLUDES += frameworks/base/include
 LOCAL_C_INCLUDES += system/core/include
-
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-
-include $(BUILD_SHARED_LIBRARY)
-
-# The audio policy is implemented on top of legacy policy code
-include $(CLEAR_VARS)
-
-#LOCAL_SRC_FILES := \
-#   AudioPolicyManager.cpp \
-#   audio_policy_hal.cpp
-
-LOCAL_SHARED_LIBRARIES := \
-    libcutils \
-    libutils \
-    libmedia
-
-LOCAL_STATIC_LIBRARIES := \
-    libaudiohw_legacy \
-    libmedia_helper \
-    libaudiopolicy_legacy
-
-LOCAL_MODULE := audio_policy.msm7x27a
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE_TAGS := optional
-
-ifeq ($(BOARD_HAVE_BLUETOOTH),true)
-  LOCAL_CFLAGS += -DWITH_A2DP
-endif
-
-LOCAL_C_INCLUDES := hardware/libhardware_legacy/audio
 
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
