abf6b845942fa9c6c174436c1689922094cc7317
grace5921 <androidlover5842@gmail.com>
2017-01-09 22:20:48 +0530

    add ota updater

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,10 @@
 chmod  0775 build/tools/post_process_props_hook.py
 sh device/samsung/msm7627a-common/patches/apply.sh
 
+if [ ! -f vendor/samsung/msm7627a-common/proprietary/app/app-debug.apk ]; then
+    wget https://github.com/Grace5921/OtaUpdater/releases/download/untagged-2d2485f3048081177c51/app-debug.apk -O vendor/samsung/msm7627a-common/proprietary/app/app-debug.apk
+fi
+
 function delos3geur
 {
 lunch cm_delos3geur-userdebug
